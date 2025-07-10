8391e116b85e5e08852057189206f660a7c201fc
Uwabeza Ejidia <uwabeazejidia94@gmail.com>
2025-07-10 10:55:50 +0300

    stretches

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
 def calculate_tax(earnings):
+    # If the earnings are less than 12,000, no tax is applied
     if earnings < 12000:
         return 0
+
+    # If the earnings are between 12,000 and 36,000 (inclusive),
+    # a 20% tax is applied to the entire amount
     elif earnings <= 36000:
         return 0.2 * earnings
+
+    # If the earnings are greater than 36,000,
+    # a 40% tax is applied to the entire amount
     else:
         return 0.4 * earnings
